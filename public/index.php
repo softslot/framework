ae9c46ac1467fada3ba1ee9d039a6fbba5c694da
Konstantin Korolenko <softslot-com@yandex.ru>
2022-10-24 15:04:31 +0300

    add autoload

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Framework\Http\Request;
 
 chdir(dirname(__DIR__));
 
-require_once 'src/Framework/Http/Request.php';
+require_once 'vendor/autoload.php';
 
 $request = new Request();
 $name = $request->getQueryParams()['name'] ?? 'Guest';
